079d52d01282aa1f175a1571a59e2c599c49a693
KillerDogs <kenybustamante2@hotmail.com>
2024-01-30 18:39:21 -0300

    theme change and main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from "@/components/Navbar"
+import { HeroEffect } from "@/components/hero-effect"
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="mx-auto max-w-5xl text-2xl gap-2 my-10">
-        <h1>Hello</h1>
+      <main className="mx-auto w-full text-2xl gap-2 -mt-20">
+        <HeroEffect />
       </main>
     </>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import { ModeToggle } from "./ModeToggle";
 import Search from "./Search";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { AvatarFallback } from "@radix-ui/react-avatar";
 
 const Navbar = () => {
   return (
@@ -9,18 +11,22 @@ const Navbar = () => {
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex gap-6 md:gap-10">
           <Link href="/" className="flex items-center space-x-2">
+            <Avatar>
+              <AvatarImage src="/logo-cartola-ia-full.png" />
+            </Avatar>
             <span className="inline-block font-bold">Cartola IA</span>
           </Link>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1 gap-3">
-            <Search />
-            <ModeToggle />
+            <Avatar>
+              {/* <AvatarImage src="/user-default-dark.jpg" /> */}
+            </Avatar>
           </nav>
         </div>
       </div>
     </header>
-  );
+  )
 };
 
 export default Navbar;
