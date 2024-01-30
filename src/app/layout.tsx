debece7da62e5f6cd3d10392cb87dd43405417f5
KillerDogs <kenybustamante2@hotmail.com>
2024-01-30 17:37:49 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next"
+import { ThemeProvider } from "@/contexts/ThemeProvider"
+import "./globals.css"
+
+export const metadata: Metadata = {
+  title: "Cartola IA",
+  description: "As melhores análises do Cartola",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
